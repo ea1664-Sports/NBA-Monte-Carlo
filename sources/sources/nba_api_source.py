ce115ef5b2b3901c2247d3eb323147727e44fbe1
ea1664-Sports <ea1664@gmail.com>
2025-10-20 20:31:14 -0400

    Delete sources/sources/nba_api_source.py

--- sources/sources/nba_api_source.py
+++
@@ -1,5 +0,0 @@
-from nba_api.stats.endpoints import leaguedashteamstats
-
-def get_nba_api_data():
-    stats = leaguedashteamstats.LeagueDashTeamStats().get_data_frames()[0]
-    return stats.to_dict(orient="records")
