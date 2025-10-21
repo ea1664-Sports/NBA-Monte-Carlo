550ada0b8e2276c9818c8842b82bd2aea3c42d91
ea1664-Sports <ea1664@gmail.com>
2025-10-21 15:21:59 -0400

    Create data_broker.py

---
+++ broker/data_broker.py
@@ -0,0 +1,19 @@
+import json, time
+from sources.espn_source import get_espn_data
+from sources.nba_api_source import get_nba_api_data
+from sources.balldontlie_source import get_balldontlie_data
+from utils.normalization import normalize_snapshot
+from utils.cache import save_cache
+
+def fetch_latest_data():
+    try:
+        data = get_nba_api_data()
+    except Exception:
+        try:
+            data = get_espn_data()
+        except Exception:
+            data = get_balldontlie_data()
+    snapshot = normalize_snapshot(data)
+    save_cache(snapshot)
+    print("Snapshot updated", time.ctime())
+    return snapshot
