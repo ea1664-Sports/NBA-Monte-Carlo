baec4383067550e4652a575f9c6eb15d51dfa178
ea1664-Sports <ea1664@gmail.com>
2025-10-21 15:39:29 -0400

    Update data_broker.py

--- broker/data_broker.py
+++ broker/data_broker.py
@@ -1,9 +1,13 @@
+import sys, os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 import json, time
 from sources.espn_source import get_espn_data
 from sources.nba_api_source import get_nba_api_data
 from sources.balldontlie_source import get_balldontlie_data
 from utils.normalization import normalize_snapshot
 from utils.cache import save_cache
+
 
 def fetch_latest_data():
     try:
@@ -13,6 +17,7 @@ def fetch_latest_data():
             data = get_espn_data()
         except Exception:
             data = get_balldontlie_data()
+
     snapshot = normalize_snapshot(data)
     save_cache(snapshot)
     print("Snapshot updated", time.ctime())
