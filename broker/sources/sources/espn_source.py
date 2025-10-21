0ce93e0dd5d105be8f1556b2bd7fe94925d35c8b
ea1664-Sports <ea1664@gmail.com>
2025-10-21 15:13:59 -0400

    Delete broker/sources/sources/espn_source.py

--- broker/sources/sources/espn_source.py
+++
@@ -1,6 +0,0 @@
-import requests
-def get_espn_data():
-    r = requests.get("https://site.api.espn.com/apis/site/v2/sports/basketball/nba/teams")
-    r.raise_for_status()
-    teams = [t["team"] for t in r.json()["sports"][0]["leagues"][0]["teams"]]
-    return teams
